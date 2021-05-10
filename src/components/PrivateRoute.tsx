817b20c38713ac57acf647481408f237c7167669
Ernest Roszak <ernest.roszak89@gmail.com>
2021-05-10 12:29:27 +0200

    refactor with eslint

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,23 +1,15 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 // import { useAuth } from "../contexts/AuthContext"
 
-export default function PrivateRoute({
-  component: Component,
-  currentUser,
-  ...rest
-}: any) {
+export default function PrivateRoute({ component: Component, currentUser, ...rest }: any) {
   // const { currentUser } = useAuth()
 
   return (
     <Route
       {...rest}
       render={(props) => {
-        return currentUser ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-        );
+        return currentUser ? <Component {...props} /> : <Redirect to="/login" />;
       }}
     ></Route>
   );
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import { auth } from "../firebase";
+import React, { useState } from 'react';
+import { auth } from '../firebase';
+import { Button } from './atoms/Button/Button';
 
 function login(email: string, password: string) {
   return auth.signInWithEmailAndPassword(email, password);
 }
 
 function SingIn() {
-  const [email, setEmail] = useState("test@gmail.com");
-  const [password, setPassword] = useState("test123");
+  const [email, setEmail] = useState('test@gmail.com');
+  const [password, setPassword] = useState('test123');
   // const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -17,25 +18,20 @@ function SingIn() {
       const result = await login(email, password);
       console.log(result);
     } catch {
-      console.log("błąd");
+      console.log('błąd');
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        name="email"
-        value={email}
-        placeholder="email"
-        onChange={(e) => setEmail(e.target.value)}
-      ></input>
+      <input name="email" value={email} placeholder="email" onChange={(e) => setEmail(e.target.value)}></input>
       <input
         name="password"
         value={password}
         placeholder="password"
         onChange={(e) => setPassword(e.target.value)}
       ></input>
-      <button type="submit">Submit</button>
+      <Button type="submit">Submit</Button>
     </form>
   );
 }
