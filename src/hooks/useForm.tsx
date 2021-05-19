a7156a9b032f23cf61ddb4c6c85ce6a293e3bbcc
Ernest Roszak <ernest.roszak89@gmail.com>
2021-05-20 00:04:15 +0200

    new hook for forms

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,60 +1,19 @@
-import { IRegistrationFormState } from 'components/molecules/Registration/Registration';
-import { useReducer } from 'react';
+import { ILoginValues } from 'components/molecules/SignIn/SignIn';
+import React, { useState } from 'react';
 
-const actionTypes = {
-  inputChange: 'INPUT CHANGE',
-  clearForm: 'CLEAR FORM',
-  consentToggle: 'CONSENT TOGGLE',
-  throwError: 'THROW ERROR',
-};
-
-const reducer = (state: IRegistrationFormState, action: any) => {
-  switch (action.type) {
-    case actionTypes.inputChange:
-      return {
-        ...state,
-        [action.field]: action.value,
-      };
-    case actionTypes.clearForm:
-      return {
-        ...action.initialState,
-      };
-    // case actionTypes.consentToggle:
-    //   return {
-    //     ...state,
-    //     consent: !state.consent,
-    //   };
-    case actionTypes.throwError:
-      return {
-        ...state,
-        error: action.errorValue,
-      };
-    default:
-      return state;
-  }
-};
-
-export const useForm = (initialState: any) => {
-  const [formValue, dispatch] = useReducer(reducer, initialState);
+const useForm = (initialState: ILoginValues) => {
+  const [formValues, setFormValues] = useState(initialState);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch({
-      field: e.target.name,
-      value: e.target.value,
-      type: actionTypes.inputChange,
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
     });
   };
-
-  const handleClearForm = () => {
-    dispatch({ type: actionTypes.clearForm, initialState });
+  return {
+    handleInputChange,
+    formValues,
   };
-  const handleThrowError = (errorMessage: string) => {
-    dispatch({ type: actionTypes.throwError, errorValue: errorMessage });
-  };
-  const handleConsent = () => {
-    dispatch({ type: actionTypes.consentToggle, initialState });
-  };
-  return { formValue, handleInputChange, handleClearForm, handleThrowError, handleConsent };
 };
 
 export default useForm;
