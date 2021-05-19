13773307a876a78de85820bb052ee85d8e6abb81
Ernest Roszak <ernest.roszak89@gmail.com>
2021-05-19 20:05:32 +0200

    added provider

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,41 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from 'assets/styles/globalStyles';
-import { theme } from 'assets/styles/theme';
-import { auth } from '../firebase';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Dashboard from 'components/Dashboard';
 import SingIn from 'components/molecules/SignIn/SignIn';
-import firebase from 'firebase';
 import PrivateRoute from 'components/PrivateRoute';
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
+import { useAuth } from 'hooks/useAuth';
 
 function Root() {
-  const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user);
-      console.log(user);
-    });
-
-    return unsubscribe;
-  }, []);
-
+  const { currentUser }: any = useAuth();
   return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <MainTemplate>
-          <Switch>
-            <Route path="/login">
-              <SingIn />
-            </Route>
-            <PrivateRoute currentUser={currentUser} component={Dashboard} path="/dashboard" />
-          </Switch>
-        </MainTemplate>
-      </ThemeProvider>
-    </Router>
+    <MainTemplate>
+      <Switch>
+        <Route path="/login">
+          <SingIn />
+        </Route>
+        <PrivateRoute currentUser={currentUser} component={Dashboard} path="/dashboard" />
+      </Switch>
+    </MainTemplate>
   );
 }
 
