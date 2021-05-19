cf426dc7158348ac52ec9d7e37d0f596133633e8
Ernest Roszak <ernest.roszak89@gmail.com>
2021-05-19 20:05:20 +0200

    added useForm hook

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,60 @@
+import { IRegistrationFormState } from 'components/molecules/Registration/Registration';
+import { useReducer } from 'react';
+
+const actionTypes = {
+  inputChange: 'INPUT CHANGE',
+  clearForm: 'CLEAR FORM',
+  consentToggle: 'CONSENT TOGGLE',
+  throwError: 'THROW ERROR',
+};
+
+const reducer = (state: IRegistrationFormState, action: any) => {
+  switch (action.type) {
+    case actionTypes.inputChange:
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+    case actionTypes.clearForm:
+      return {
+        ...action.initialState,
+      };
+    // case actionTypes.consentToggle:
+    //   return {
+    //     ...state,
+    //     consent: !state.consent,
+    //   };
+    case actionTypes.throwError:
+      return {
+        ...state,
+        error: action.errorValue,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useForm = (initialState: any) => {
+  const [formValue, dispatch] = useReducer(reducer, initialState);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      field: e.target.name,
+      value: e.target.value,
+      type: actionTypes.inputChange,
+    });
+  };
+
+  const handleClearForm = () => {
+    dispatch({ type: actionTypes.clearForm, initialState });
+  };
+  const handleThrowError = (errorMessage: string) => {
+    dispatch({ type: actionTypes.throwError, errorValue: errorMessage });
+  };
+  const handleConsent = () => {
+    dispatch({ type: actionTypes.consentToggle, initialState });
+  };
+  return { formValue, handleInputChange, handleClearForm, handleThrowError, handleConsent };
+};
+
+export default useForm;
